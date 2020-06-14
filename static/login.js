9c6c97d83212daf0b1798c43568446318292bf57
dkoceja <dkoceja@live.unc.edu>
2020-06-14 02:32:40 -0400

    added alert to login page

--- static/login.js
+++ static/login.js
@@ -3,6 +3,8 @@ function require() {
     var input = document.forms["username"]["personId"].value;
     for (var i = 0; i < input.length; i++) {
         if (input[i] == "/" || input[i] == " " || input[i] == "$" || input[i] == "%" || input[i] == "&") {
+            alert("Invalid username. Please do not use special characters (!,@,#) or spaces");  	
+
             return false;
         }
     }
