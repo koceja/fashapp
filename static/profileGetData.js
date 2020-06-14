84f7926381dad8b30717b23cd131c4b732aecfe1
dkoceja <dkoceja@live.unc.edu>
2020-06-14 01:50:08 -0400

    take 2 on this bug fix

--- static/profileGetData.js
+++ static/profileGetData.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
         const urls = $(".card-columns").html()
-        $(".card-columns").empty()
-        if (urls == "") {
-            return
-        } else {
-            $(urls).appendTo(".card-columns")
-        }
+        $(urls).appendTo(".card-columns")
 })
