3bd0bcb1616f73e9c6dddd9772aebae7913699f6
dkoceja <dkoceja@live.unc.edu>
2020-06-18 17:58:30 -0400

    added color

--- static/animateLike.js
+++ static/animateLike.js
@@ -4,5 +4,8 @@
 
     
    $(".row").on("click", "div.card-link", function() {
-	$(this).css("color", "red");
+    $(this).css("color", "red");
+    $(this).animate({marginBottom: "1.35rem"}, 200);
+    $(this).animate({marginBottom: "1.35rem"}, 200);
+
 });
